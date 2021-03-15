fdf0a69538ab4c61bd7dee4b8b3de6b88a4b4665
Yuskeee <60994304+Yuskeee@users.noreply.github.com>
2021-03-15 15:56:23 -0300

    Update Entidade.cpp

--- source/Entidade.cpp
+++ source/Entidade.cpp
@@ -7,7 +7,7 @@ pos(pos), vel(vel), rect(rect), vulnerabilidade(), idTextura(idT), idSprite(idS)
 Entidade::~Entidade(){
 }
 
-void Entidade::desenhar(Graphics &gameGraphics){
+void Entidade::draw(Graphics &gameGraphics){
 gameGraphics.drawSprite(idSprite);
 }
 
