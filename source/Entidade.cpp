dbc17572d6b12eab3e019799b4ef0e79a8cafdd3
Yuskeee <60994304+Yuskeee@users.noreply.github.com>
2021-03-15 15:36:11 -0300

    Add files via upload

---
+++ source/Entidade.cpp
@@ -0,0 +1,20 @@
+#include <Entidade.h>
+
+Entidade::Entidade(sf::Vector2 pos, sf::Vector2 vel, sf::Vector2 , TextureID idT, SpriteID idS):
+pos(pos), vel(vel), rect(rect), idTextura(idT), idSprite(idS){
+}
+
+Entidade::~Entidade(){
+}
+
+void Entidade::desenhar(Graphics &gameGraphics){
+gameGraphics.drawSprite(idSprite);
+}
+
+const Vetor2F Entidade::getPos() const{
+return pos;
+}
+
+const Vetor2F Entidade::getRect() const{
+return rect;
+}
