a1416212f8429bd4fde5566147982492625c1d58
Yuskeee <60994304+Yuskeee@users.noreply.github.com>
2021-03-15 15:42:54 -0300

    Update Entidade.cpp

--- source/Entidade.cpp
+++ source/Entidade.cpp
@@ -1,7 +1,7 @@
 #include "Entidade.h"
 
 Entidade::Entidade(sf::Vector2 pos, sf::Vector2 vel, sf::Vector2 , TextureID idT, SpriteID idS):
-pos(pos), vel(vel), rect(rect), idTextura(idT), idSprite(idS){
+pos(pos), vel(vel), rect(rect), vulnerabilidade(), idTextura(idT), idSprite(idS){
 }
 
 Entidade::~Entidade(){
